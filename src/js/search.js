fd7bf34f623a2aa7b8cd5887c7338a0f0f952722
Martina Neureiter <mneureiter.mmt-b2016@fh-salzburg.ac.at>
2018-01-31 10:33:01 +0100

    feat: autocomplete search field with ajax

--- src/js/search.js
+++ src/js/search.js
@@ -1,4 +1,27 @@
+let dataList = document.getElementById('country--autocomplete')
 let userInput = document.getElementById('user-input')
+
+let request = new XMLHttpRequest()
+request.onreadystatechange = function(response) {
+  if(request.readyState === 4) {//done
+    if(request.status === 200){ //OK
+      let jsonOptions = JSON.parse(request.responseText)
+      jsonOptions.forEach(function(item) {
+        let option = document.createElement('option')
+        option.value = item.name;
+        dataList.appendChild(option)
+      })
+      userInput.placeholder = "country"
+    }
+    else{
+      userInput.placeholder = "Could not load datalist options"
+    }
+  }
+}
+request.open('GET', 'json/countries_search.json', true)
+request.send()
+
+
 userInput.addEventListener("keydown", function(e){
   if (e.keyCode === 13){ //enter key
     e.preventDefault();
@@ -7,7 +30,7 @@ userInput.addEventListener("keydown", function(e){
 })
 
 function search(e){
-  var countrydata = JSON.parse(countries)
+  let countrydata = JSON.parse(countries)
   countrydata.forEach(function(element) {
     if(e.target.value.toLowerCase() == element.name.toLowerCase()){
       window.location.href = "/comparison.html?country=" + element.code
