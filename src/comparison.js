6edcafb8b73604a9d39472aabefa0515d593b5ad
Clemens Gasser <cgasser.mmt-b2016@fh-salzburg.ac.at>
2018-01-31 16:52:39 +0100

    feat: d3 fillgauge animation

--- src/comparison.js
+++ src/comparison.js
@@ -2,6 +2,7 @@ import './sass/style.scss';
 import Logo from './images/logo_blue.png';
 import './js/compare.js';
 import './json/countries_search.json';
+import './js/fillgauge.js'
 
 
 document.getElementById('logo_blue').src = Logo;
