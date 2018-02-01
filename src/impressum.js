affd6e74f9c0363b9dcbf5904413fbc9636e9d32
Martina Neureiter <mneureiter.mmt-b2016@fh-salzburg.ac.at>
2018-02-01 10:16:06 +0100

    feat: you can only click on countries which are available

---
+++ src/impressum.js
@@ -0,0 +1,7 @@
+import './sass/style.scss';
+import Logo from './images/logo_blue.png';
+import './js/form.js';
+
+
+
+document.getElementById('logo_blue').src = Logo;
--- src/js/map.js
+++ src/js/map.js
@@ -1,4 +1,4 @@
-var country;
+let country;
 $(function () {
   $('#map').vectorMap({
     map: 'world_mill',
@@ -15,8 +15,25 @@ $(function () {
         }
     },
     onRegionClick: function onRegionClick(event, code) {
-      window.location.href = "comparison.html?country=" + code;
-      country = code;
+      let avaiable = checkIfAvailable(code)
+      if(avaiable === true) {
+        window.location.href = "comparison.html?country=" + code;
+        country = code;
+      }   
+      else{
+        console.log(`The country ${code} is not avaiable.`)
+      }
     }
   });
+  
+  function checkIfAvailable(code) {
+    let avaiable = true
+    const noneAvailableCountries = ["AX", "AS", "AD", "AI", "AQ", "AW", "BS", "BM", "BV", "IO", "KY", "CX", "CC", "CK", "FK", "FO", "GF", "PF", "TF", "GI", "GL", "GP", "GU", "GG", "HM", "VA", "HK", "IM", "JE", "KI", "LI", "MO", "MH", "MR", "YT", "FM", "MS", "NR", "AN", "NC", "NU", "NF", "MP", "PW", "PN", "RE", "SH", "PM", "WS", "SM", "SB", "GS", "SJ", "TW", "TK", "TO", "TC", "TV", "VU", "VG", "VI", "WF", "EH"]
+    noneAvailableCountries.forEach(function(element) {
+      if(element == code) {
+        avaiable = false
+      }
+    })
+    return avaiable
+  }
 });
